d72fccaa3b3f3d99ff556df67a9ea19722ae4b1f
nuelxelence <nuelxelence@gmail.com>
2022-09-20 04:59:54 +0100

    settings

--- bankloan/settings.py
+++ bankloan/settings.py
@@ -32,19 +32,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-=x6!qx0qc2vr(s@2r_v6st18fb@d&i!(m%k+a9)mg2xoj-8kt9'
-#SECRET_KEY= config('SECRET_KEY')
+
 SECRET_KEY=os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-#DEBUG = config('DEBUG',cast=bool)
+
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['financialbank.herokuapp.com','127.0.0.1']
-#if not DEBUG:
- #   ALLOWED_HOSTS +=[os.environ.get('ALLOWED_HOST')]
+ALLOWED_HOSTS = ['financialbank.herokuapp.com','127.0.0.1','www.simsudresources.com','simsudresources.com']
 
 
 # Application definition
