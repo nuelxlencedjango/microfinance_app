a9eac978f7d2394dac60bb050e90ce44e7836d59
nuelxelence <nuelxelence@gmail.com>
2022-11-18 07:45:10 +0100

    messages  changes

--- loanApp/views.py
+++ loanApp/views.py
@@ -164,7 +164,8 @@ def UserLoanHistory(request):
     except Exception as e:
 
         messages.success(request, 'You dont have any loan histry yet.')
-        return render(request, "loanApp/user_loan_history.html")
+        
+        #return render(request, "loanApp/user_loan_history.html")
 
     #loans = loanRequest.objects.filter(customer=request.user)
     #get_info = CustomerLoan.objects.filter(customer=request.user)
