703b08a88ea3957e259b8848d58477e6b933771e
nuelxelence <nuelxelence@gmail.com>
2023-01-03 12:59:01 +0100

    update

--- bankloan/settings.py
+++ bankloan/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
+     "corsheaders",
 
     'cloudinary',
     'account',
@@ -72,10 +72,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     
-    
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
